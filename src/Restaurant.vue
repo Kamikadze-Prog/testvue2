33300cbf48eaeae4c210b3aef54dae4c1a681c8d
Kamikadze-Prog <42613145+Kamikadze-Prog@users.noreply.github.com>
2019-02-12 16:30:12 +0200

    Create Restaurant.vue

---
+++ src/Restaurant.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p> {{msg}} </p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+      msg: '444'
+      }
+    },
+}
+</script>
+
+<style  lang="scss">
+
+</style>
